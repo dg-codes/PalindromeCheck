06de75f5f8c39ced924562e01cfe2dd3fa2ce66a
Dragos Godeanu <dragos.godeanu@live.co.uk>
2018-06-06 22:39:58 +0100

    Updated GetPalindromes, fixed GeneratePalindrome
    Implemented GetPalindromes
    
    Added null or empty input handling logic to GeneratePalindrome.
    
    All tests passed.

--- PalindromeCheck/Processors/PalindromeProcessor.cs
+++ PalindromeCheck/Processors/PalindromeProcessor.cs
@@ -12,11 +12,31 @@ namespace PalindromeCheck.Processors
     {
         public IList<ISubstring> GetPalindromes(string input)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(input)) return null;
+
+            int pivotingLenght = 2 * input.Length + 1;
+            var palindromes = new List<ISubstring>();
+
+            for (int i = 0; i < pivotingLenght; i++)
+            {
+                var palindrome = (Substring)GeneratePalindrome(input, i);
+
+                if (palindrome == null || string.IsNullOrWhiteSpace(palindrome.Text)) continue;
+
+                if (palindrome.Length > 1)
+                {
+                    palindromes.Add(palindrome);
+                }
+
+            }
+
+            return palindromes;
         }
 
         public ISubstring GeneratePalindrome(string input, int pivot)
         {
+            if (string.IsNullOrWhiteSpace(input)) return null;
+
             int left = pivot / 2;
             int right = left + pivot % 2;
 
